9f5a100ae686dca14aee201968bd7a6e28bdf100
Min <miniz.project@gmail.com>
2014-01-27 14:48:17 +0800

    Update

---
+++ src/com/it2299/reincoast/servlet/MemberSignOutServlet.java
@@ -0,0 +1,30 @@
+package com.it2299.reincoast.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberSignOutServlet
+ */
+@WebServlet("/MemberSignOutServlet")
+public class MemberSignOutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberSignOutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
